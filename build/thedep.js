c2f692dbc2fa26e6f76f3e68f2d14135f4be544c
Chris Weed <chris@workgrid.com>
2018-05-08 09:31:29 -0500

    Added built for example purposes

---
+++ build/thedep.js
@@ -0,0 +1,14 @@
+(window["webpackJsonpdeptest"] = window["webpackJsonpdeptest"] || []).push([["thedep"],{
+
+/***/ "../the-dep/Print.js":
+/*!***************************!*\
+  !*** ../the-dep/Print.js ***!
+  \***************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("!function(e,t){ true?module.exports=t(__webpack_require__(/*! react */ \"../the-dep/node_modules/react/index.js\")):undefined}(window,function(e){return function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}return n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{configurable:!1,enumerable:!0,get:r})},n.r=function(e){Object.defineProperty(e,\"__esModule\",{value:!0})},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,\"a\",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p=\"\",n(n.s=1)}([function(t,n){t.exports=e},function(e,t,n){\"use strict\";n.r(t);var r=n(0),o=n.n(r);console.log(\"The print.js module has loaded...\"),t.default=function(e){return function(e){if(null==e)throw new TypeError(\"Cannot destructure undefined\")}(e),o.a.createElement(\"p\",null,\"You printed something\")}}])});\n\n//# sourceURL=webpack://deptest/../the-dep/Print.js?");
+
+/***/ })
+
+}]);
