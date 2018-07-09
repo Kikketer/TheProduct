e6e519bcbc2d586d06dd0e170dfb42782c15b656
Chris Weed <chris@workgrid.com>
2018-07-09 17:24:37 -0500

    Using emotion and pulling in the standard library

--- src/App.js
+++ src/App.js
@@ -3,38 +3,12 @@ import React, { Component } from 'react';
 class App extends Component {
   constructor () {
     super()
-    this.state = {Print: null}
   }
-
-  componentDidMount() {
-    setTimeout(() => {
-      import(/*webpackPrefetch: true*/ 'the-dep/Print').then(
-        Print => {
-          console.log('Got the print library!')
-          this.setState({
-            Print: Print.default
-          })
-        }
-      )
-    }, 1000)
-  }
-
-  // loadPrint() {
-  //   // This is if the library has the "import" statement (not in this example)
-  //   getPrint().then(p => {
-  //     console.log('Got Print, setting state: ', p.default)
-  //     this.setState({Print: p.default})
-  //   }).catch(err => {
-  //     console.log('Error loading the print module: ', err)
-  //   })
-  // }
 
   render() {
-    const { Print } = this.state
     return (
       <div>
-        <p>You should see the Print component below:</p>
-        {Print && <Print/>}
+        <p>This is a vanilla web page</p>
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: 'development',
   entry: ['./src/index.js'],
   output: {
-    library: 'deptest',
+    library: 'product',
     libraryTarget: 'umd',
     umdNamedDefine: true,
     filename: '[name].js',
